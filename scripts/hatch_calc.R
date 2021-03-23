5b0eb251425fdb2f94be00c66858f6ba578c5bbb
lieker <l.e.vlaar@uva.nl>
2021-03-23 08:11:43 +0100

    script adapted for trendline between egg density and hatching

--- scripts/hatch_calc.R
+++ scripts/hatch_calc.R
@@ -7,15 +7,22 @@ library(plotrix)
 
 
 
-hatch_calc <- function(dpi = 10) {
+hatch_calc <- function(hraw = "input/hatching_raw.csv",
+                       dpi = 10) {
   source("scripts/hatch_raw.R")
-  raw <- hatch_raw()
+  raw <- hatch_raw(hatch_raw = hraw)
+  
+  if (dpi == "all")
+  raw$hatchingpercent <- ((as.numeric(as.character(raw$juvs)) - as.numeric(as.character(raw$juvs_t0))) / as.numeric(as.character(raw$eggs_t0)) * 100)
+  h_calc <- subset(raw, select = -c(juvs, juvs_t0))
+  return(h_calc)
+  
+  if (class(dpi) == "integer")
+  
   raw <- raw[raw$t == dpi,]
 
-  #calculate hatching percentage
   raw$hatchingpercent <- ((as.numeric(as.character(raw$juvs)) - as.numeric(as.character(raw$juvs_t0))) / as.numeric(as.character(raw$eggs_t0)) * 100)
-  
-  #remove unnecessary rows
+
   h_calc <- subset(raw, select = -c(juvs, eggs_t0, juvs_t0))
   
   return(h_calc)
